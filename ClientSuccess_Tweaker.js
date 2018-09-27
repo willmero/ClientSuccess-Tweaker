c889a92b4f1a1a8a6deaf1daa820fbfe8160b876
Will Mero <will@roadmunk.com>
2018-09-27 14:30:12 -0400

    Deprecates code that will add in Contract Value Fixes overlapping success cycle headers Brings up feature request custom attribute to Client header

--- ClientSuccess_Tweaker.js
+++ ClientSuccess_Tweaker.js
@@ -18,18 +18,26 @@ async function getClientSuccess(path){
 
 // add a placeholder loading indicator for Contract Value
 var topBarInjector = setInterval(function(){
-	if($('.disp-pre-body').find('table tr').length > 0){
-		clearInterval(topBarInjector);
-		$('.disp-pre-body').find('table tr').append('<td class="details-data details-data-indented"><ul><li><span class="title" style="width: 102px">Contract Value</span><span class="detail" id="contract-value-value">Loading...</span></li></ul></td>');
+	if($('.disp-pre-body').find('table tr').length > 0 && $('#featureRequestHolder').length == 0){
+		$('.disp-pre-body').find('table').append('<tr><td id="featureRequestHolder" colspan="7" class="details-data details-data-indented" style="width: 100%; padding-top: 5px;"><ul><li><span class="title">Feature Requests</span><span class="detail" id="feature-request-container-inner"></span></li></ul></td></tr>');
 		topBarInjected = true;
 	}
 }, 300);
 
+var featureRequestWindowSurfacer = setInterval(function(){
+	if($('.Select-menu-outer').length > 0){
+		$('.Select-menu-outer').css('z-index', 1000);
+	}
+}, 200);
 
 var contactContainerListener = setInterval(async function(){
     if($('.contact-item').length > 0 && $('.all-contacts').not('.support-tool-patched').length > 0){
     	$('.all-contacts').not('.support-tool-patched').addClass('support-tool-patched');
         console.log('Contacts loaded!');
+        if($('svg[name="key"]').length === 0 && $('#key-contact-alert').length === 0){
+        	// check to make sure that the client has a key contact
+        	$('div[ng-if="!SuccessPageCtrl.loading"]').prepend('<div id="key-contact-alert" style="width: 100%; background-color: red; color: white; padding: 8px; font-size: 24px" align="center">This client does not have a key contact</div>');	
+        }
         // get the client ID
         var currentURL = window.location.pathname.split('/');
         if(currentURL[1] == 'cs' && currentURL[2] == 'success' && !isNaN(parseInt(currentURL[3]))){
@@ -63,23 +71,16 @@ var contactContainerListener = setInterval(async function(){
 
 var clientCustomFieldListener = setInterval(function(){
 	console.log('Checking custom...');
-	if($('.custom-fields-app').find( "label:contains('Contract Value')" ).length > 0 && topBarInjected === true){
+	if($('.custom-fields-app').find( "label:contains('Feature Requests')" ).length > 0 && topBarInjected === true && $('#featureRequestHolder').find('div[ng-switch-when="multiselectlist"]').length == 0) {
 		console.log('Injecting custom');
-		$('#contract-value-value').text($('.custom-fields-app').find( "label:contains('Contract Value')" ).parent().find('.content-editable').text());
+		//$('.custom-fields-app').find( "label:contains('Feature Requests')" ).parent().find('.Select-input').appendTo('body');
+		$('.custom-fields-app').find( "label:contains('Feature Requests')" ).parent().find('div[ng-switch-when="multiselectlist"]').appendTo('#feature-request-container-inner')
+		//$('#contract-value-value').text(Math.ceil(parseInt($('.custom-fields-app').find( "label:contains('Contract Value')" ).parent().find('.content-editable').text())));
 	}
 }, 1000);
 
-// fix for overlapping SuccessCycle headers
-var successCycleHeaderFix = setInterval(function(){
-	if($('.stage-navigator').length > 0 && $('.stage-navigator__next').length > 0 && $('.stage-navigator__prev').length > 0){
-		$('.stage-navigator').height('28px');
-		$('.stage-navigator__next').css('padding-top', '14px');
-		$('.stage-navigator__prev').css('padding-top', '14px');
-	}
-}, 300);
-
-// Expired Account Banner
-var clientTypeExpiredChecker = setInterval(function(){
+var persistentIntervalChecker = setInterval(function(){
+	// Expired Account Banner
 	if($('li[ng-if="SuccessPageCtrl.client.clientSegmentId"]').length > 0) {
 		if($('li[ng-if="SuccessPageCtrl.client.clientSegmentId"]').find('.detail').text() == "Expired"){
 			if($('#expired-client-container').length == 0){
@@ -87,4 +88,18 @@ var clientTypeExpiredChecker = setInterval(function(){
 			}
 		}
 	}
+
+	// fix for overlapping SuccessCycle headers
+	if($('.stage-navigator').length > 0 && $('.stage-navigator__next').length > 0 && $('.stage-navigator__prev').length > 0){
+		$('.stage-navigator').height('28px');
+		$('.stage-navigator__next').css('padding-top', '14px');
+		$('.stage-navigator__prev').css('padding-top', '14px');
+	}
+
+	// blank ClientName fix (provices a clickable link for the CSM)
+	jQuery('.nameCell__name').each(function(){
+		if(jQuery(this).text() == ' '){
+			jQuery(this).text('null'); // replaces blank name with null name
+		}
+	});
 }, 300);
